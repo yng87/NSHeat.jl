a0556d0b5351597402ace0a20a55351dc113c3e2
kei-87 <yanagi@hep-th.phys.s.u-tokyo.ac.jp>
2019-01-31 23:36:23 +0900

    fix bug, make set_core_params

--- src/setup.jl
+++ src/setup.jl
@@ -1,12 +1,13 @@
 module Setup
 
+export set_core_params
+
 include("./NeutronStar.jl")
 include("./PhysicalConstants.jl")
 
 using .NeutronStar
 using DelimitedFiles
 using Dierckx
-using Interpolations
 
 function read_eos_core(path_eos_core::String)
     #"Rho, Press, nbar, Ye, Ymu, Yn, Yp, Yla, Ysm, Ys0, Ysp, mstp, mstn, mstla, mstsm, msts0, mstsp"
@@ -22,8 +23,7 @@ function set_core_params(eos::Array{Float64,2}, tov::Array{Float64,2}, del_slice
     # Reverse is needed for x to be ascending
     #Ye_intp = interpolate(eos[:,4], BSpline(Linear()))
     Ye_spl = Spline1D(reverse(eos[:,3]), reverse(eos[:,4]))
-    Ye_spl = Spline1D(reverse(eos[:,3]), reverse(eos[:,4]))
-    Ymu_spl = Spline1D(reverse(eos[:,3]), reverse(eos[:,5]))
+    Ymu_spl = Spline1D(reverse(eos[:,3]), reverse(eos[:,5]), k=1) # k>=2 does not work since Ymu = 0 near core-crust boundary
     Yn_spl = Spline1D(reverse(eos[:,3]), reverse(eos[:,6]))
     Yp_spl = Spline1D(reverse(eos[:,3]), reverse(eos[:,7]))
     mstp_spl = Spline1D(reverse(eos[:,3]), reverse(eos[:,12]))
@@ -31,30 +31,37 @@ function set_core_params(eos::Array{Float64,2}, tov::Array{Float64,2}, del_slice
     
 
     # Get quantity from TOV table, as a function of radius
+    # First, get only core params from TOV
     r_data = tov[:,2]
-    r_core = r_data[1]:del_slice:r_data[end]
-    @show r_data
-    @show tov[:,3]
-    nB_spl = Spline1D(r_data, tov[:,3]) #Baryon density 1/fm^3
-    @show nB_spl(100.0)
-    ephi_spl = Spline1D(r_data, exp.(tov[:,7])) #exp(phi)
+    nB_min_in_core = min(eos[:,3]...)
+    idx_tov_core = tov[:,3] .> nB_min_in_core
+    r_core_data = tov[:,2][idx_tov_core]
 
+    # User defined slices
+    r_core = r_core_data[1]:del_slice:r_core_data[end]
+
+    # Then, interpolate 
+    nB_spl = Spline1D(r_core_data, tov[:,3][idx_tov_core]) #Baryon density 1/fm^3
+    ephi_spl = Spline1D(r_core_data, exp.(tov[:,7][idx_tov_core])) #exp(phi)
+
+    # Compute particle quantities
     nB_arr = nB_spl.(r_core)
-    nn_arr = nB_arr .* Yn_spl.(r_core)
-    np_arr = nB_arr .* Yp_spl.(r_core)
-    ne_arr = nB_arr .* Ye_spl.(r_core)
-    nmu_arr = nB_arr .* Ymu_spl.(r_core)
+    nn_arr = nB_arr .* Yn_spl.(nB_arr)
+    np_arr = nB_arr .* Yp_spl.(nB_arr)
+    ne_arr = nB_arr .* Ye_spl.(nB_arr)
+    nmu_arr = nB_arr .* Ymu_spl.(nB_arr)
 
     kFn_arr = ( (3*pi^2) .* nn_arr ).^(1.0/3.0)
     kFp_arr = ( (3*pi^2) .* np_arr ).^(1.0/3.0)
     kFe_arr = ( (3*pi^2) .* ne_arr ).^(1.0/3.0)
     kFmu_arr = ( (3*pi^2) .* nmu_arr ).^(1.0/3.0)
-
+    
+pp    # Intialize StarCoreParams
     core = StarCoreParams(r_core,
                           ephi_spl.(r_core),
                           nB_arr,
-                          mstn_spl.(r_core),
-                          mstp_spl.(r_core),
+                          mstn_spl.(nB_arr),
+                          mstp_spl.(nB_arr),
                           sqrt.(1 .+ kFe_arr.^2 ./me^2),
                           sqrt.(1 .+ kFmu_arr.^2 ./mmu^2),
                           kFn_arr,
