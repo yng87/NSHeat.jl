f4d6ffd2f3a9a42d82cb6702987292f184992dd8
kei-87 <yanagi@hep-th.phys.s.u-tokyo.ac.jp>
2019-02-03 16:38:12 +0900

    heating solve, electron has bug?

--- src/ODESolvers.jl
+++ src/ODESolvers.jl
@@ -1,6 +1,6 @@
 module ODESolvers
 
-export cooling
+export cooling, heating
 
 push!(LOAD_PATH, "./")
 include("./PhysicalConstants.jl")
@@ -13,6 +13,7 @@ using HeatCapacity
 using NeutrinoLum
 using PhotonLum
 using SuperfluidGaps
+using SpinDown
 
 function cooling(model::ModelParams, core::StarCoreParams, env::EnvelopeParams, var::StarVariables,
                  tspan::Tuple{Float64,Float64}, reltol=1e-10, abstol=1e-10)
@@ -39,6 +40,53 @@ function cooling(model::ModelParams, core::StarCoreParams, env::EnvelopeParams, 
     u0 = var.Tinf
     set_vn(model, core, var)
     set_vp(model, core, var)
+    prob = ODEProblem(f, u0, tspan)
+    sol = solve(prob, CVODE_Adams(), reltol, abstol)
+
+    return sol
+    
+end
+
+function heating(model::ModelParams, core::StarCoreParams, env::EnvelopeParams, var::StarVariables,
+                 tspan::Tuple{Float64,Float64}, reltol=1e-10, abstol=1e-10)
+    
+    #u = [Tinf, eta_e_inf, eta_mu_inf]
+    function f(u,p,t)
+        var.t = t #yr
+        var.Tinf = u[1]
+        var.eta_e_inf = u[2] #erg
+        var.eta_e_inf = u[3] #erg
+        set_Tlocal(core, var)
+        set_vn(model, core, var)
+        set_vp(model, core, var)
+        set_Omega(model, var)
+        set_Omega_dot(model, var)
+        #Heat capacity
+        C = get_Ce(model, core, var) + get_Cmu(model, core, var) + get_Cn(model, core, var) + get_Cp(model, core, var)
+        #Neutrino luminosity
+        Lnu = L_murca_n_e(model, core, var, false) + L_murca_n_mu(model, core, var, false) + L_murca_p_e(model, core, var, false) + L_murca_p_mu(model, core, var, false)
+        if lowercase(model.SFtype_n) != "normal"
+            Lnu += L_PBF_n(model, core, var)
+        end
+        if lowercase(model.SFtype_p) != "normal"
+            Lnu += L_PBF_p(model, core, var)
+        end
+        #Do not forget yrTosec!
+        Rate_e = Rate_volume_murca_n_e(model, core, var, false) + Rate_volume_murca_p_e(model, core, var, false)
+        Rate_mu = Rate_volume_murca_n_mu(model, core, var, false) + Rate_volume_murca_p_mu(model, core, var, false)
+
+        du1 = (-Lnu/C - L_photon(model, env, var)/C + var.eta_e_inf*Rate_e/C + var.eta_mu_inf*Rate_mu/C) * yrTosec
+        du2 = (-model.Znpe * Rate_e - model.Znp*Rate_mu + 2*model.Wnpe*var.Omega*var.Omega_dot) * yrTosec
+        du3 = (-model.Znp * Rate_e - model.Znpmu*Rate_mu + 2*model.Wnpmu*var.Omega*var.Omega_dot) *yrTosec
+        return [du1, du2, du3]
+    end
+
+    u0 = [var.Tinf, var.eta_e_inf, var.eta_mu_inf]
+    set_vn(model, core, var)
+    set_vp(model, core, var)
+    set_Omega(model, var)
+    set_Omega_dot(model, var)
+
     prob = ODEProblem(f, u0, tspan)
     sol = solve(prob, CVODE_Adams(), reltol, abstol)
 
--- src/SpinDown.jl
+++ src/SpinDown.jl
@@ -3,18 +3,19 @@ module SpinDown
 export set_Omega, set_Omega_dot
 
 push!(LOAD_PATH, "./")
+include("./PhysicalConstants.jl")
 
 using NeutronStar
 
 function set_Omega(model::ModelParams, var::StarVariables)
     # 1/s
-    var.Omega = 2*pi / sqrt(model.P0^2 + 2*model.Pnow*model.Pdotnow*var.t)
+    var.Omega = 2*pi / sqrt(model.P0^2 + 2*model.Pnow*model.Pdotnow*var.t*yrTosec)
     return nothing
 end
 
 function set_Omega_dot(model::ModelParams, var::StarVariables)
     # 1/s^2
-    var.Omega_dot = -2*pi*model.Pnow*model.Pdotnow / (sqrt(model.P0^2 + 2*model.Pnow*model.Pdotnow*var.t))^3
+    var.Omega_dot = -2*pi*model.Pnow*model.Pdotnow / (sqrt(model.P0^2 + 2*model.Pnow*model.Pdotnow*var.t*yrTosec))^3
     return nothing
 end
 
