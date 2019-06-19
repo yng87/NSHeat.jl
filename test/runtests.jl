3633bda3d08107f9ebe0e49b3b7de0142a6b6320
basspale <k.yanagi07@gmail.com>
2019-06-19 15:19:34 +0900

    correct test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ function test()
     
     model, core, env, var = setup("sample.ini")
     sol1 = heating(model, core, env, var)
+    model, core, env, var = setup("sample.ini")
     sol2 = cooling(model, core, env, var)
 
     return (sol1[end]==:Success) && (sol2[end]==:Success)
